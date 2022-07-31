f37c4d0cd4fa5f8ed21bb210728d23e9c1e8e1f2
Mu Ye <emailyemu@gmail.com>
2022-07-31 00:09:01 -0700

    Night and horizon

--- src/pages/sunset/index.tsx
+++ src/pages/sunset/index.tsx
@@ -19,10 +19,10 @@ const Sky = styled.div`
   width: 100%;
   height: 50%;
   z-index: 0;
-  background: -webkit-linear-gradient(bottom, rgba(249,251,240,1) 10%, rgba(215,253,254,1) 20%, rgba(167,222,253,1) 40%, rgba(110,175,255,1) 100%); 
-  background: -moz-linear-gradient(bottom, rgba(249,251,240,1) 10%, rgba(215,253,254,1) 20%, rgba(167,222,253,1) 40%, rgba(110,175,255,1) 100%); 
-  background: -ms-linear-gradient(bottom, rgba(249,251,240,1) 10%, rgba(215,253,254,1) 20%, rgba(167,222,253,1) 40%, rgba(110,175,255,1) 100%); 
-  background: linear-gradient(bottom, rgba(249,251,240,1) 10%, rgba(215,253,254,1) 20%, rgba(167,222,253,1) 40%, rgba(110,175,255,1) 100%); 
+  background: -webkit-linear-gradient(bottom, rgba(249, 251, 240, 1) 10%, rgba(215, 253, 254, 1) 20%, rgba(167, 222, 253, 1) 40%, rgba(110, 175, 255, 1) 100%); 
+  background: -moz-linear-gradient(bottom, rgba(249, 251, 240, 1) 10%, rgba(215, 253, 254, 1) 20%, rgba(167, 222, 253, 1) 40%, rgba(110, 175, 255, 1) 100%); 
+  background: -ms-linear-gradient(bottom, rgba(249, 251, 240, 1) 10%, rgba(215, 253, 254, 1) 20%, rgba(167, 222, 253, 1) 40%, rgba(110, 175, 255, 1) 100%); 
+  background: linear-gradient(bottom, rgba(249, 251, 240, 1) 10%, rgba(215, 253, 254, 1) 20%, rgba(167, 222, 253, 1) 40%, rgba(110, 175, 255, 1) 100%); 
 `;
 
 const Sun = styled.div<SunProps>`
@@ -53,7 +53,7 @@ const Sunset = styled.div<SunProps>`
   width: 100%;
   height: 50%;
   z-index: 1;
-  opacity: ${props => props.y / window.innerHeight - 0.25};
+  opacity: ${props => props.y / window.innerHeight - 0.2};
   background-repeat: no-repeat;
   background: -webkit-radial-gradient(${props => `${props.x}px ${props.y}px`}, circle, ${props => props.gradient});
   background: -moz-radial-gradient(${props => `${props.x}px ${props.y}px`}, circle, ${props => props.gradient});
@@ -66,10 +66,49 @@ const Sea = styled.div`
   height: 50%;
   top: 50%;
   z-index: 2;
-  background: -webkit-linear-gradient(bottom, rgba(0,25,45,1) 0%,rgba(14,71,117,1) 35%, rgba(26,126,174,1) 70%, rgba(62,168,220,1) 100%); 
-  background: -moz-linear-gradient(bottom, rgba(0,25,45,1) 0%,rgba(14,71,117,1) 35%, rgba(26,126,174,1) 70%, rgba(62,168,220,1) 100%); 
-  background: -ms-linear-gradient(bottom, rgba(0,25,45,1) 0%,rgba(14,71,117,1) 35%, rgba(26,126,174,1) 70%, rgba(62,168,220,1) 100%); 
-  background: linear-gradient(bottom, rgba(0,25,45,1) 0%,rgba(14,71,117,1) 35%, rgba(26,126,174,1) 70%, rgba(62,168,220,1) 100%); 
+  background-repeat: no-repeat;
+  background: -webkit-linear-gradient(bottom, rgba(0, 25, 45, 1) 0%, rgba(14, 71, 117, 1) 35%, rgba(26, 126, 174, 1) 70%, rgba(62, 168, 220, 1) 100%); 
+  background: -moz-linear-gradient(bottom, rgba(0, 25, 45, 1) 0%, rgba(14, 71, 117, 1) 35%, rgba(26, 126, 174, 1) 70%, rgba(62, 168, 220, 1) 100%); 
+  background: -ms-linear-gradient(bottom, rgba(0, 25, 45, 1) 0%, rgba(14, 71, 117, 1) 35%, rgba(26, 126, 174, 1) 70%, rgba(62, 168, 220, 1) 100%); 
+  background: linear-gradient(bottom, rgba(0, 25, 45, 1) 0%, rgba(14, 71, 117, 1) 35%, rgba(26, 126, 174, 1) 70%, rgba(62, 168, 220, 1) 100%); 
+`;
+
+const Horizon = styled.div<CursorPosition>`
+  position: absolute;
+  width: 100%;
+  height: 50%;
+  bottom: 50%;
+  z-index: 4;
+  opacity: ${props => props.y <= (window.innerHeight * 0.5) ? 0 : (props.y / window.innerHeight - 0.5)};
+  background-repeat: no-repeat;
+  background: -webkit-linear-gradient(bottom, rgba(57, 167, 255, 1) 0%, rgba(13, 98, 245, 1) 20%, rgba(0, 11, 22, 0.1) 60%);
+  background: -moz-linear-gradient(bottom, rgba(57, 167, 255, 1) 0%, rgba(13, 98, 245, 1) 20%, rgba(0, 11, 22, 0.1) 60%);
+  background: -ms-linear-gradient(bottom, rgba(57, 167, 255, 1) 0%, rgba(13, 98, 245, 1) 20%,rgba(0, 11, 22, 0.1) 60%);
+  background: linear-gradient(bottom, rgba(57, 167, 255, 1) 0%, rgba(13, 98, 245, 1) 20%, rgba(0, 11, 22, 0.1) 60%);
+`;
+
+const NightSky = styled.div<CursorPosition>`
+  position: absolute;
+  width: 100%;
+  height: 50%;
+  top: 0;
+  left: 0;
+  cursor: default;
+  background-color: black;
+  opacity: ${props => props.y <= (window.innerHeight * 0.5) ? 0 : (props.y / window.innerHeight - 0.5)};
+  z-index: 3;
+`;
+
+const NightSea = styled.div<CursorPosition>`
+  position: absolute;
+  width: 100%;
+  height: 50%;
+  bottom: 0;
+  left: 0;
+  cursor: default;
+  background-color: black;
+  opacity: ${props => props.y / window.innerHeight};
+  z-index: 3;
 `;
 
 const SunsetPage: React.FC = () => {
@@ -115,10 +154,16 @@ const SunsetPage: React.FC = () => {
   return (
     <Wrapper onMouseMove={handleMouseMove}>
       <Sky />
+      <NightSky x={cursor.x} y={cursor.y} />
+
       <Sun x={cursor.x} y={cursor.y} gradient={sunGradient} />
       <Sunlight x={cursor.x} y={cursor.y} gradient={sunlightGradient} />
       <Sunset x={cursor.x} y={cursor.y} gradient={sunsetGradient} />
+
+      <Horizon x={cursor.x} y={cursor.y} />
+
       <Sea />
+      <NightSea x={cursor.x} y={cursor.y} />
     </Wrapper>
   );
 };
